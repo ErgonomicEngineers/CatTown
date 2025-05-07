c154442bd7ab88bc52c2252a9734fa5133cc2ce8
Sophia Zheng <Sophizheng0722@berkeley.edu>
2025-05-07 14:10:04 -0500

    Added new page: Posture Report

---
+++ src/data/reportdata.js
@@ -0,0 +1,12 @@
+export const sessions = [
+  { day: "Mon", start: "9:00AM", end: "11:00AM", score: 8 },
+  { day: "Mon", start: "1:00PM", end: "3:00PM", score: 5 },
+  { day: "Tue", start: "10:30AM", end: "12:00PM", score: 3 },
+  { day: "Wed", start: "9:00AM", end: "10:00AM", score: 7 },
+  { day: "Wed", start: "3:15PM", end: "5:00PM", score: 2 },
+  { day: "Thu", start: "11:00AM", end: "1:30PM", score: 6 },
+  { day: "Fri", start: "2:00PM", end: "4:00PM", score: 4 },
+  { day: "Sat", start: "12:00PM", end: "2:00PM", score: 9 },
+  { day: "Sun", start: "9:00AM", end: "12:00PM", score: 1 },
+];
+  
